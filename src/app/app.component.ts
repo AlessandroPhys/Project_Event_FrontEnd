babcb225499fd7f0a0573720926ca13d4f61058d
Alessandro <gabialessandroledesma@gmail.com>
2025-07-06 03:34:27 -0300

    Add component.html, adjust backend route, and update app.component.ts logic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { EventService } from './services/event.service'; // Asegúrate de que la ruta es correcta
 
 @Component({
   selector: 'app-root',
@@ -8,4 +9,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'event-app-front';
+
+  events: any[] = [];
+
+  constructor(private services: EventService) { }
+
+  ngOnInit(): void {
+    this.getEvents();
+  }
+
+  getEvents(): void {
+    this.services.getEvents().subscribe(
+      (response: any) => {
+        console.log('Respuesta del backend:', response); // se ve en la consola del navegador
+        this.events = response;
+      },
+      (error: any) => {
+        console.error('Error fetching events:', error);
+      }
+    );
+
+  }
 }
