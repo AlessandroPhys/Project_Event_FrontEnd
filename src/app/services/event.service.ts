79ec23c50612466537c8de38307cbc7dbbb1b8ae
Lautaro <perezlautaro63@gmail.com>
2025-07-08 19:52:58 -0300

    EventService now supports full CRUD operations

--- src/app/services/event.service.ts
+++ src/app/services/event.service.ts
@@ -11,11 +11,28 @@ export class EventService {
 
   constructor(private http: HttpClient) { }
 
-  getEvents(): Observable<any[]>{//Asumimos que devuelve un array de eventos
+  getEvents(): Observable<any[]> {//Asumimos que devuelve un array de eventos
     return this.http.get<any[]>(`${this.apiUrl}/api/v1/events`);
   }
 
   getEventById(id:number): Observable<any> {
     return this.http.get<any>(`${this.apiUrl}/api/v1/events/${id}`);
   }
+
+  createEvent(eventData: any): Observable<any> {
+    return this.http.post<any>(`${this.apiUrl}/api/v1/events`, eventData);
+  }
+
+  updateEventFull(id: number, eventData: any): Observable<any> {
+    return this.http.put<any>(`${this.apiUrl}/api/v1/events/${id}`, eventData);
+  }
+
+  updateEventPartial(id: number, eventData: any): Observable<any> {
+    return this.http.patch<any>(`${this.apiUrl}/api/v1/events/${id}`, eventData);
+  }
+
+  deleteEvent(id: number): Observable<any> {
+    return this.http.delete<any>(`${this.apiUrl}/api/v1/events/${id}`);
+  }
+
 }
