f3e9fc7d1a1e5e339698e08e989423fe874caad9
Alessandro <gabialessandroledesma@gmail.com>
2025-08-01 11:11:48 -0300

    Side nav left

--- src/app/components/nav-bar/nav-bar.component.ts
+++ src/app/components/nav-bar/nav-bar.component.ts
@@ -1,15 +1,16 @@
 import { Component, ViewChild } from '@angular/core';
 import { MatSidenav } from '@angular/material/sidenav';
-import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-nav-bar',
-  templateUrl: './nav-bar.component.html',
   standalone: false,
-  styleUrls: ['./nav-bar.component.css'],
+  templateUrl: './nav-bar.component.html',
+  styleUrls: ['./nav-bar.component.css']
 })
 export class NavBarComponent {
   @ViewChild('sidenav') sidenav!: MatSidenav;
 
-  constructor(public authService: AuthService) {}
+  toggleSidenav() {
+    this.sidenav.toggle();
+  }
 }
